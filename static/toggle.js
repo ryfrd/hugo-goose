cea8ba42cd51e8b019d56d990a336479345c8a0a
ryfrd <jdysmcl@tutanota.com>
2023-07-04 22:46:24 +0100

    theme toggle???

--- static/toggle.js
+++ static/toggle.js
@@ -1,13 +1,16 @@
 function toggleTheme() {
+
   var stylesheet = document.getElementById('stylesheet');
   var themeButton = document.getElementById('themeButton');
 
-  if (stylesheet.getAttribute('href') === 'dark.css') {
-    stylesheet.setAttribute('href', 'light.css');
+  if (stylesheet.getAttribute('href') === '/drk.css') {
+
+    stylesheet.setAttribute('href', '/lght.css');
     themeButton.textContent = 'dark';
 
   } else {
-    stylesheet.setAttribute('href', 'dark.css');
+
+    stylesheet.setAttribute('href', '/drk.css');
     themeButton.textContent = 'light';
 
   }
